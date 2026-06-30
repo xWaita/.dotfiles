028c1b14a3ebf2a4e7c7bfb12a6d71de772ba411
xWaita <victor21898@gmail.com>
2026-06-30 14:12:41 +1000

    nvim fix configs

--- nvim/.config/nvim/lua/shared/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/shared/nvim-treesitter.lua
@@ -1,8 +1,11 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter').setup({
-            ensure_installed = { "json", "hyprlang" }
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { 'json', 'hyprlang' },
+            highlight = { enable = true },
         })
     end,
 }
